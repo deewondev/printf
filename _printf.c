094a89f4b97ca36aa23fd85844f4b47b1571cbf9
Godspower <godspy18@gmail.com>
2023-07-24 22:06:34 -0700

    updated print format

--- _printf.c
+++ _printf.c
@@ -30,10 +30,10 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flags = print_flags(format, &i);
+			width = print_width(format, &i, list);
+			precision = print_precision(format, &i, list);
+			size = print_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
