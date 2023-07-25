c6508e9625797242395a4617af3deaf6b930880c
deewondev <deewondev@gmail.com>
2023-07-25 05:32:38 +0100

    main header file

--- main.h
+++ main.h
@@ -20,7 +20,6 @@
 
 /**
  * struct fmt - Struct op
- *
  * @fmt: The format.
  * @fn: The function associated.
  */
@@ -30,10 +29,8 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-
 /**
  * typedef struct fmt fmt_t - Struct op
- *
  * @fmt: The format.
  * @fm_t: The function associated.
  */
